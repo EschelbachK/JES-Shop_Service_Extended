79592b9ef4fede9e5f591f5c84614ca635c0d0bd
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2025-07-16 13:47:05 +0200

    Handle Optional in ShopService and add OrderStatus when creating order

--- src/main/java/org/example/ShopService.java
+++ src/main/java/org/example/ShopService.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class ShopService {
@@ -10,17 +11,29 @@ public class ShopService {
 
     public Order addOrder(List<String> productIds) {
         List<Product> products = new ArrayList<>();
+
+        // Produkt-IDs durchgehen und Optional prüfen
         for (String productId : productIds) {
-            Product productToOrder = productRepo.getProductById(productId);
-            if (productToOrder == null) {
+
+            // Rückgabe ist jetzt Optional
+            Optional<Product> productOpt = productRepo.getProductById(productId);
+            if (productOpt.isEmpty()) {
                 System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
                 return null;
             }
-            products.add(productToOrder);
+            // Produkt aus dem Optional extrahieren
+            products.add(productOpt.get());
         }
 
-        Order newOrder = new Order(UUID.randomUUID().toString(), products);
+        // Order hat jetzt zusätzlich den Bestellstatus als dritten Parameter
+        Order newOrder = new Order(
+                // eindeutige ID erzeugen
+                UUID.randomUUID().toString(),
+                products,
 
+                // Standardstatus beim Erstellen einer neuen Bestellung
+                OrderStatus.PROCESSING
+        );
         return orderRepo.addOrder(newOrder);
     }
 }
