0391dd45691e98459afcb9bdc6c0e99c65cffde6
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2025-07-16 15:35:59 +0200

    Add orderDate timestamp to Order and update ShopService.addOrder

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -1,12 +1,13 @@
 package org.example;
 
 import lombok.With;
-
+import java.time.Instant;
 import java.util.List;
 
 public record Order(
         String id,
         List<Product> products,
-        @With OrderStatus status
+        @With OrderStatus status,
+        @With Instant orderDate
 ) {
 }
